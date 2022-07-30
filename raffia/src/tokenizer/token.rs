3a82641f7db9eb85cdc88d341e461a0bbf47a263
Pig Fang <g-plane@hotmail.com>
2022-07-30 11:18:20 +0800

    refactor: order tokens

--- raffia/src/tokenizer/token.rs
+++ raffia/src/tokenizer/token.rs
@@ -23,124 +23,63 @@ pub struct LineComment<'a> {
 #[derive(Clone, Debug, Spanned)]
 pub enum Token<'a> {
     Eof,
-    Ident(Ident<'a>),
-    Number(Number<'a>),
-    Dimension(Dimension<'a>),
-    Percentage(Percentage<'a>),
-    Str(Str<'a>),
-    Url(Url<'a>),
-    Hash(Hash<'a>),
-    DollarVar(DollarVar<'a>),
-    AtLBraceVar(AtLBraceVar<'a>),
-    AtKeyword(AtKeyword<'a>),
-    Function(Function<'a>),
-    Colon(Colon),
-    ColonColon(ColonColon),
-    LParen(LParen),
-    RParen(RParen),
-    LBracket(LBracket),
-    RBracket(RBracket),
-    LBrace(LBrace),
-    RBrace(RBrace),
-    Solidus(Solidus),
-    Comma(Comma),
-    Semicolon(Semicolon),
-    Dot(Dot),
-    Plus(Plus),
-    GreaterThan(GreaterThan),
-    Tilde(Tilde),
-    Bar(Bar),
-    BarBar(BarBar),
     Ampersand(Ampersand),
     Asterisk(Asterisk),
-    Equal(Equal),
-    TildeEqual(TildeEqual),
+    AsteriskEqual(AsteriskEqual),
+    AtKeyword(AtKeyword<'a>),
+    AtLBraceVar(AtLBraceVar<'a>),
+    Bar(Bar),
+    BarBar(BarBar),
     BarEqual(BarEqual),
     CaretEqual(CaretEqual),
+    Colon(Colon),
+    ColonColon(ColonColon),
+    Comma(Comma),
+    Dimension(Dimension<'a>),
     DollarEqual(DollarEqual),
-    AsteriskEqual(AsteriskEqual),
+    DollarVar(DollarVar<'a>),
+    Dot(Dot),
+    Equal(Equal),
+    Function(Function<'a>),
+    GreaterThan(GreaterThan),
+    Hash(Hash<'a>),
     HashLBrace(HashLBrace),
+    Ident(Ident<'a>),
+    LBrace(LBrace),
+    LBracket(LBracket),
+    LParen(LParen),
+    Number(Number<'a>),
+    Percentage(Percentage<'a>),
+    Plus(Plus),
     PlusUnderscore(PlusUnderscore),
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Ident<'a> {
-    pub name: Cow<'a, str>,
-    pub raw: &'a str,
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Function<'a> {
-    pub name: Ident<'a>,
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Number<'a> {
-    pub value: f64,
-    pub raw: &'a str,
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Dimension<'a> {
-    pub value: Number<'a>,
-    pub unit: Ident<'a>,
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Percentage<'a> {
-    pub value: Number<'a>,
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Str<'a> {
-    pub value: Cow<'a, str>,
-    pub raw: &'a str,
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct BadStr<'a> {
-    pub value: Cow<'a, str>,
-    pub raw: &'a str,
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct UrlRaw<'a> {
-    pub value: Cow<'a, str>,
-    pub raw: &'a str,
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub enum UrlValue<'a> {
+    RBrace(RBrace),
+    RBracket(RBracket),
+    RParen(RParen),
+    Semicolon(Semicolon),
+    Solidus(Solidus),
     Str(Str<'a>),
-    Raw(UrlRaw<'a>),
+    Tilde(Tilde),
+    TildeEqual(TildeEqual),
+    Url(Url<'a>),
 }
 
 #[derive(Clone, Debug, Spanned)]
-pub struct Url<'a> {
-    pub value: UrlValue<'a>,
+pub struct Ampersand {
     pub span: Span,
 }
 
 #[derive(Clone, Debug, Spanned)]
-pub struct Hash<'a> {
-    pub value: Cow<'a, str>,
-    /// raw string with beginning `#` char
-    pub raw: &'a str,
-    /// raw string without beginning `#` char
-    pub raw_without_hash: &'a str,
+pub struct Asterisk {
     pub span: Span,
 }
 
 #[derive(Clone, Debug, Spanned)]
-pub struct DollarVar<'a> {
+pub struct AsteriskEqual {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct AtKeyword<'a> {
     pub ident: Ident<'a>,
     pub span: Span,
 }
@@ -152,83 +91,9 @@ pub struct AtLBraceVar<'a> {
 }
 
 #[derive(Clone, Debug, Spanned)]
-pub struct AtKeyword<'a> {
-    pub ident: Ident<'a>,
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Colon {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct ColonColon {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct LParen {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct RParen {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct LBracket {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct RBracket {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct LBrace {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct RBrace {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Solidus {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Comma {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Semicolon {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Dot {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Plus {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct GreaterThan {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Tilde {
+pub struct BadStr<'a> {
+    pub value: Cow<'a, str>,
+    pub raw: &'a str,
     pub span: Span,
 }
 
@@ -243,26 +108,6 @@ pub struct BarBar {
 }
 
 #[derive(Clone, Debug, Spanned)]
-pub struct Ampersand {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Asterisk {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct Equal {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
-pub struct TildeEqual {
-    pub span: Span,
-}
-
-#[derive(Clone, Debug, Spanned)]
 pub struct BarEqual {
     pub span: Span,
 }
@@ -273,12 +118,66 @@ pub struct CaretEqual {
 }
 
 #[derive(Clone, Debug, Spanned)]
+pub struct Colon {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct ColonColon {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Comma {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Dimension<'a> {
+    pub value: Number<'a>,
+    pub unit: Ident<'a>,
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
 pub struct DollarEqual {
     pub span: Span,
 }
 
 #[derive(Clone, Debug, Spanned)]
-pub struct AsteriskEqual {
+pub struct DollarVar<'a> {
+    pub ident: Ident<'a>,
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Dot {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Equal {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Function<'a> {
+    pub name: Ident<'a>,
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct GreaterThan {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Hash<'a> {
+    pub value: Cow<'a, str>,
+    /// raw string with beginning `#` char
+    pub raw: &'a str,
+    /// raw string without beginning `#` char
+    pub raw_without_hash: &'a str,
     pub span: Span,
 }
 
@@ -288,6 +187,107 @@ pub struct HashLBrace {
 }
 
 #[derive(Clone, Debug, Spanned)]
+pub struct Ident<'a> {
+    pub name: Cow<'a, str>,
+    pub raw: &'a str,
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct LBrace {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct LBracket {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct LParen {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Number<'a> {
+    pub value: f64,
+    pub raw: &'a str,
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Percentage<'a> {
+    pub value: Number<'a>,
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Plus {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
 pub struct PlusUnderscore {
     pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct RBrace {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct RBracket {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct RParen {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Semicolon {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Solidus {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Str<'a> {
+    pub value: Cow<'a, str>,
+    pub raw: &'a str,
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Tilde {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct TildeEqual {
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct Url<'a> {
+    pub value: UrlValue<'a>,
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub struct UrlRaw<'a> {
+    pub value: Cow<'a, str>,
+    pub raw: &'a str,
+    pub span: Span,
+}
+
+#[derive(Clone, Debug, Spanned)]
+pub enum UrlValue<'a> {
+    Str(Str<'a>),
+    Raw(UrlRaw<'a>),
 }
