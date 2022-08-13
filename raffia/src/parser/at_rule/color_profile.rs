42060d3f6229881a1e3aba104401b778a7a36c0e
Pig Fang <g-plane@hotmail.com>
2022-08-13 09:36:58 +0800

    refactor

--- raffia/src/parser/at_rule/color_profile.rs
+++ raffia/src/parser/at_rule/color_profile.rs
@@ -1,20 +1,12 @@
 use super::Parser;
-use crate::{
-    ast::*,
-    error::{Error, ErrorKind, PResult},
-};
+use crate::{ast::*, error::PResult};
 
 impl<'cmt, 's: 'cmt> Parser<'cmt, 's> {
     pub(super) fn parse_color_profile_prelude(&mut self) -> PResult<ColorProfilePrelude<'s>> {
-        match self.parse_interpolable_ident()? {
+        match self.parse_dashed_ident()? {
             InterpolableIdent::Literal(ident) if ident.name.eq_ignore_ascii_case("device-cmyk") => {
                 Ok(ColorProfilePrelude::DeviceCmyk(ident))
             }
-            // this should be recoverable
-            InterpolableIdent::Literal(ident) if !ident.name.starts_with("--") => Err(Error {
-                kind: ErrorKind::ExpectDashedIdent,
-                span: ident.span,
-            }),
             ident => Ok(ColorProfilePrelude::DashedIdent(ident)),
         }
     }
