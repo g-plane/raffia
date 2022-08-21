323b59f659853d33aa845b7eee0934d98f0ee633
Pig Fang <g-plane@hotmail.com>
2022-08-21 15:33:19 +0800

    add error messages

--- raffia/src/error.rs
+++ raffia/src/error.rs
@@ -1,19 +1,11 @@
 use crate::pos::Span;
-use std::{error, fmt::Display};
+use std::fmt::Display;
 
 #[derive(Clone, Debug)]
 pub struct Error {
     pub kind: ErrorKind,
     pub span: Span,
 }
-
-impl Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "")
-    }
-}
-
-impl error::Error for Error {}
 
 #[derive(Clone, Debug)]
 pub enum ErrorKind {
@@ -67,6 +59,66 @@ pub enum ErrorKind {
     InvalidRatioDenominator,
     ExpectMediaFeatureName,
     ExpectDashedIdent,
+}
+
+impl Display for ErrorKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Unexpected(expected, actual) => {
+                write!(f, "expect token `{expected}`, but `{actual}` received")
+            }
+
+            Self::UnknownToken => write!(f, "unknown token"),
+            Self::InvalidNumber => write!(f, "invalid number"),
+            Self::InvalidEscape => write!(f, "invalid escape"),
+            Self::InvalidHash => write!(f, "invalid hash token"),
+            Self::ExpectRightBraceForLessVar => write!(f, "`}}` for Less variable is expected"),
+            Self::UnexpectedLinebreak => write!(f, "unexpected linebreak"),
+            Self::UnexpectedEof => write!(f, "unexpected end of file"),
+
+            Self::UnexpectedWhitespace => write!(f, "unexpected whitespace"),
+            Self::ExpectSimpleSelector => write!(f, "simple selector is expected"),
+            Self::InvalidIdSelectorName => write!(f, "invalid ID selector name"),
+            Self::ExpectTypeSelector => write!(f, "type selector is expected"),
+            Self::ExpectIdSelector => write!(f, "ID selector is expected"),
+            Self::ExpectWqName => write!(f, "WqName is expected"),
+            Self::ExpectAttributeSelectorMatcher => {
+                write!(f, "attribute selector matcher is expected")
+            }
+            Self::ExpectAttributeSelectorValue => write!(f, "attribute selector value is expected"),
+            Self::ExpectComponentValue => write!(f, "component value is expected"),
+            Self::ExpectSassExpression => write!(f, "Sass expression is expected"),
+            Self::ExpectDedentOrEof => write!(f, "dedentation or end of file is expected"),
+            Self::ExpectString => write!(f, "string is expected"),
+            Self::ExpectUrl => write!(f, "URL is expected"),
+            Self::UnexpectedTemplateInCss => write!(f, "template isn't allowed in CSS"),
+            Self::ExpectMediaFeatureComparison => write!(f, "media feature comparison is expected"),
+            Self::ExpectMediaAnd => write!(f, "media query `and` is expected"),
+            Self::ExpectMediaOr => write!(f, "media query `or` is expected"),
+            Self::ExpectMediaNot => write!(f, "media query `not` is expected"),
+            Self::ExpectContainerConditionAnd => write!(f, "container condition `and` is expected"),
+            Self::ExpectContainerConditionOr => write!(f, "container condition `or` is expected"),
+            Self::ExpectContainerConditionNot => write!(f, "container condition `not` is expected"),
+            Self::ExpectStyleConditionAnd => write!(f, "style condition `and` is expected"),
+            Self::ExpectStyleConditionOr => write!(f, "style condition `or` is expected"),
+            Self::ExpectStyleConditionNot => write!(f, "style condition `not` is expected"),
+            Self::ExpectStyleQuery => write!(f, "style query is expected"),
+            Self::ExpectSassKeyword(keyword) => write!(f, "Sass keyword `{keyword}` is expected"),
+            Self::InvalidAnPlusB => write!(f, "invalid An+B syntax"),
+            Self::ExpectInteger => write!(f, "an integer is expected"),
+            Self::ExpectUnsignedInteger => write!(f, "unsigned integer is expected"),
+            Self::ExpectImportantAnnotation => write!(f, "`!important` is expected"),
+
+            Self::TryParseError => unreachable!(),
+            Self::CSSWideKeywordDisallowed => {
+                write!(f, "using CSS wide keyword as identifier is disallowed")
+            }
+            Self::UnknownKeyframeSelectorIdent => write!(f, "unknown keyframe selector"),
+            Self::InvalidRatioDenominator => write!(f, "ratio denominator is invalid"),
+            Self::ExpectMediaFeatureName => write!(f, "media feature name is expected"),
+            Self::ExpectDashedIdent => write!(f, "dashed identifier is expected"),
+        }
+    }
 }
 
 pub type PResult<T> = Result<T, Error>;
