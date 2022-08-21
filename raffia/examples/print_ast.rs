233013dc76aaf667ee13f37b5484182d32c28ebd
Pig Fang <g-plane@hotmail.com>
2022-08-21 16:26:11 +0800

    add pretty errors in example

--- raffia/examples/print_ast.rs
+++ raffia/examples/print_ast.rs
@@ -1,10 +1,18 @@
+use codespan_reporting::{
+    diagnostic::{Diagnostic, Label},
+    files::SimpleFile,
+    term::{
+        self,
+        termcolor::{ColorChoice, StandardStream},
+    },
+};
 use raffia::{ast::Stylesheet, Parser, Syntax};
 use std::{env, fs, path::Path};
 
 fn main() {
     let arg = env::args().skip(1).next().unwrap();
     let path = Path::new(&arg);
-    let file = fs::read_to_string(&path).unwrap();
+    let code = fs::read_to_string(&path).unwrap();
 
     let syntax = match path.extension().and_then(|ext| ext.to_str()) {
         Some("scss") => Syntax::Scss,
@@ -12,6 +20,19 @@ fn main() {
         Some("less") => Syntax::Less,
         _ => Syntax::Css,
     };
-    let ast = Parser::new(&file, syntax).parse::<Stylesheet>().unwrap();
-    println!("{:#?}", ast);
+
+    let mut parser = Parser::new(&code, syntax);
+    let result = parser.parse::<Stylesheet>();
+    match result {
+        Ok(ast) => println!("{:#?}", ast),
+        Err(error) => {
+            let file = SimpleFile::new(path.file_name().unwrap().to_str().unwrap(), &code);
+            let diagnostic = Diagnostic::error()
+                .with_message(error.kind.to_string())
+                .with_labels(vec![Label::primary((), error.span.start..error.span.end)]);
+            let writer = StandardStream::stderr(ColorChoice::Always);
+            let config = term::Config::default();
+            term::emit(&mut writer.lock(), &config, &file, &diagnostic).unwrap();
+        }
+    }
 }
