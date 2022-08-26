4308f92789fd8f257a444df137682b38420d5a4f
Pig Fang <g-plane@hotmail.com>
2022-08-26 18:12:13 +0800

    make config serializable

--- raffia/src/config.rs
+++ raffia/src/config.rs
@@ -1,5 +1,10 @@
+#[cfg(feature = "serialize")]
+use serde::Serialize;
+
 /// Supported syntax.
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
+#[cfg_attr(feature = "serialize", derive(Serialize))]
+#[cfg_attr(feature = "serialize", serde(rename_all = "camelCase"))]
 pub enum Syntax {
     #[default]
     Css,
