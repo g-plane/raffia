b7d67d5ecb618011057ca011917448cc6b429458
Pig Fang <g-plane@hotmail.com>
2022-09-06 09:52:51 +0800

    serialize `None` as `null` in WASM

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,4 +1,5 @@
 use raffia::{ast::Stylesheet, Parser};
+use serde::Serialize;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(js_name = parseStylesheet)]
@@ -6,7 +7,10 @@ pub fn parse_stylesheet(source: String, syntax: JsValue) -> Result<JsValue, JsVa
     let syntax = serde_wasm_bindgen::from_value(syntax)?;
     let mut parser = Parser::new(&source, syntax);
     match parser.parse::<Stylesheet>() {
-        Ok(ast) => serde_wasm_bindgen::to_value(&ast).map_err(JsValue::from),
+        Ok(ast) => {
+            let serializer = serde_wasm_bindgen::Serializer::new().serialize_missing_as_null(true);
+            ast.serialize(&serializer).map_err(JsValue::from)
+        }
         Err(error) => Err(serde_wasm_bindgen::to_value(&(
             &error,
             error.kind.to_string(),
