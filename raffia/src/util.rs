73ce446cc5739305ad9fcd66cf17be44cc0b89eb
李瑞丰🤡 <lirf@lenovocloud.com>
2022-09-25 21:50:24 +0800

    feat(util): add revert and revert-layer to CSS-wide keywords.

--- raffia/src/util.rs
+++ raffia/src/util.rs
@@ -7,6 +7,8 @@ pub fn is_css_wide_keyword(s: &str) -> bool {
     s.eq_ignore_ascii_case("initial")
         || s.eq_ignore_ascii_case("inherit")
         || s.eq_ignore_ascii_case("unset")
+        || s.eq_ignore_ascii_case("revert")
+        || s.eq_ignore_ascii_case("revert-layer")
 }
 
 pub trait LastOfNonEmpty<T> {
