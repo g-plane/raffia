864500d7ef199fbc1eff0265e08832af41c439a8
Pig Fang <g-plane@hotmail.com>
2023-08-23 16:40:05 +0800

    fix parsing hash in component value

--- raffia/src/parser/value.rs
+++ raffia/src/parser/value.rs
@@ -176,8 +176,9 @@ impl<'cmt, 's: 'cmt> Parser<'cmt, 's> {
             Token::Percentage(..) => self.parse().map(ComponentValue::Percentage),
             Token::Hash(..) => {
                 if self.syntax == Syntax::Less {
-                    self.try_parse(Parser::parse_less_maybe_mixin_call_or_with_lookups)
-                        .or_else(|_| self.parse().map(ComponentValue::HexColor))
+                    self.try_parse(HexColor::parse)
+                        .map(ComponentValue::HexColor)
+                        .or_else(|_| self.parse_less_maybe_mixin_call_or_with_lookups())
                 } else {
                     self.parse().map(ComponentValue::HexColor)
                 }
