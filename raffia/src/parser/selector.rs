06b5e6d3f5fe515fce56c937f0731d51bef6a3d9
Bartek Iwańczuk <biwanczuk@gmail.com>
2026-06-08 11:50:57 +0200

    Address review: replace inner matches! with children length check

--- raffia/src/parser/selector.rs
+++ raffia/src/parser/selector.rs
@@ -1237,9 +1237,7 @@ impl<'cmt, 's: 'cmt> Parse<'cmt, 's> for PseudoElementSelector<'s> {
                     {
                         let selector_list = input.parse::<SelectorList>()?;
                         let is_single_compound_selector = match &*selector_list.selectors {
-                            [ComplexSelector { children, .. }] => {
-                                matches!(&**children, [ComplexSelectorChild::CompoundSelector(..)])
-                            }
+                            [ComplexSelector { children, .. }] => children.len() == 1,
                             _ => false,
                         };
                         if !is_single_compound_selector {
